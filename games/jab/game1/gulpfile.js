1836020d6bf66064be46aa584e28874226cc7c61
Juan Antonio Breña Moral <bren@juanantonio.info>
2016-08-07 20:29:31 +0100

    Gulp running for the first Game

---
+++ games/jab/game1/gulpfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const gulp = require("gulp");
+const browserify = require("browserify");
+const del = require("del");
+const concat = require("gulp-concat");
+const source = require("vinyl-source-stream");
+
+gulp.task("clean", () => del("dist"));
+
+gulp.task("js", ["clean"], () => {
+    return browserify("src/app.js")
+        .transform("babelify", { presets: ["es2015", "react"] })
+        .bundle()
+        .pipe(source("app.js"))
+        .pipe(gulp.dest("dist/scripts"));
+});
+
+gulp.task("html",["clean"], () => {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task("watch", function() {
+  gulp.watch(["src/**", "gulpfile.js"], ["default"]);
+});
+
+gulp.task("prod", () => {
+    process.env.NODE_ENV = "production";
+});
+
+gulp.task("dist", ["prod", "default"]);
+
+gulp.task("default", ["js", "html"]);
