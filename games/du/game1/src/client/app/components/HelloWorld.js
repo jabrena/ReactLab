2ba27581ea5a9cae9fc68507210196d4cc7111a7
Daniel Upton <daniel.upton29@gmail.com>
2016-08-08 20:18:04 +0100

    Simple setup for react app. Added sateless Hello World component to be used within App.

---
+++ games/du/game1/src/client/app/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, {PropTypes} from 'react';
+
+const HelloWorld = props => {
+    return (
+        <div>
+            <p>Hello World! From a react component!</p>
+        </div>
+    );
+};
+
+export default HelloWorld;
