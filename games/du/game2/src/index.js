db549970edeb1fa5be9c9beb99e1f7bb56383642
Daniel Upton <daniel.upton29@gmail.com>
2016-08-20 21:35:00 +0100

    cloned the correct branch from my app. Then removed remote from git.

--- games/du/game2/src/index.js
+++ games/du/game2/src/index.js
@@ -1,1 +1,22 @@
-// Set up your application entry point here...
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LogonPage from './containers/LogonPage';
+
+require('./styles/common.css');
+require('../node_modules/toastr/build/toastr.min.css');
+ 
+const App = () => (
+  <MuiThemeProvider>
+    <LogonPage />
+  </MuiThemeProvider>
+);
+ 
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
