429e8fc6cbbb60df4564451c32505e0ebe5a918d
Sergio Ortiz Crespo <sergiooc@gmail.com>
2016-09-22 10:03:21 +0200

    1 - example

--- games/soc/game1/app.js
+++ games/soc/game1/app.js
@@ -1,16 +1,20 @@
 'use strict';
 
-//var React = require("react");
-//var ReactDOM = require("react-dom");
+//var React = require('react');
+//var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
     displayName: 'HelloWorld',
 
+
+    getInitialState: function getInitialState() {
+        return { hello: 'Hello World - with this.state' };
+    },
     render: function render() {
         return React.createElement(
             'h1',
             null,
-            'Hello World'
+            this.state.hello
         );
     }
 });
--- games/soc/game1/app.jsx
+++ games/soc/game1/app.jsx
@@ -1,10 +1,14 @@
-//var React = require("react");
-//var ReactDOM = require("react-dom");
+//var React = require('react');
+//var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
+
+	getInitialState: function(){
+		return { hello: 'Hello World - with this.state'}
+	},
     render: function () {
         return (
-            <h1>Hello World</h1>
+            <h1>{this.state.hello}</h1>
         );
     }
 });
