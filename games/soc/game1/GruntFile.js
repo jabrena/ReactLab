43ca5decbbab063c170b288071dc5d83432dda29
Sergio Ortiz Crespo <sergiooc@gmail.com>
2016-09-22 17:44:10 +0200

    location

--- games/soc/game1/GruntFile.js
+++ games/soc/game1/GruntFile.js
@@ -1,20 +1,42 @@
 module.exports = function(grunt) {
-
-    grunt.loadNpmTasks('grunt-babel');
     
     grunt.initConfig({
-        babel: {
-            options: {
-                sourceMap: true,
-                presets: ['es2015','react']
-            },
-            dist: {
-                files: {
-                    './app.js': './app.jsx'
-                }
+        pkg: grunt.file.readJSON('package.json'),
+        //distFolder: '../../../www/games/soc/game1',
+
+        watch: {
+          browserify: {
+            files: ['src/js/**/*.js'],
+            tasks: ['browserify']
+          }
+        },
+
+        // babel: {
+        //     options: {
+        //         sourceMap: true,
+        //         presets: ['es2015','react']
+        //     },
+        //     dist: {
+        //         files: {
+        //             './app.js': './app.jsx'
+        //         }
+        //     }
+        // }
+
+         browserify: {
+              dist: {
+                options: {
+                   transform: [['babelify', {presets: ['es2015', 'react']}]]
+                },        
+                src: ['src/js/app.js'],
+                dest: 'dist/js/app.js',
+              }
             }
-        }
     });
+
+
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-browserify');
 
     grunt.registerTask('default', ['babel']);
 
